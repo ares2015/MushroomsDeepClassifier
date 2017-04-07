1917694c818ddf7f19d8b4c29738fca977128c92
Oliver Eder <eder@essential-data.sk>
2017-04-07 13:17:11 +0200

    New data.

--- src/main/java/com/mushroomsDeepClassifier/Classifier.java
+++ src/main/java/com/mushroomsDeepClassifier/Classifier.java
@@ -49,18 +49,18 @@ public class Classifier {
     }
 
     protected static final Logger log = LoggerFactory.getLogger(Classifier.class);
-    protected static int height = 100;
-    protected static int width = 100;
+    protected static int height = 400;
+    protected static int width = 400;
     protected static int channels = 3;
     protected static int numExamples = 11;
-    protected static int numLabels = 2;
-    protected static int batchSize = 20;
+    protected static int numLabels = 4;
+    protected static int batchSize = 10;
 
     protected static long seed = 42;
     protected static Random rng = new Random(seed);
     protected static int listenerFreq = 1;
     protected static int iterations = 1;
-    protected static int epochs = 50;
+    protected static int epochs = 7;
     protected static double splitTrainTest = 0.8;
     protected static int nCores = 2;
     protected static boolean save = false;
